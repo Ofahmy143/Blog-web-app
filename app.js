e54e4c9780f55b55d941db258f2ad0a1889bb414
Omar Fahmy <Ofahmy1234@gmail.com>
2022-09-07 11:30:05 +0200

    Add the compose root to every subroot

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const https = require('https');
+const { post } = require('request');
 const request = require('request');
 
 app.use(express.urlencoded({extended:true}));
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 app.set('view engine','ejs');
 app.use(express.json());
 
-let blogHeader = [];
-let blogText = [];
+let posts = [];
+
 
 
 app.listen(3000,function(){
@@ -19,39 +20,64 @@ app.listen(3000,function(){
 
 
 app.get("/",function(req,res){
-    res.render("MainPage" , {blogHeader:blogHeader , blogText:blogText});
+    res.render("MainPage" , {posts:posts});
 })
 
-app.get("/blogs",function(req,res){
-    res.send("Yellooooo")
+
+
+app.post("/compose",function(req,res){
+    let post = {
+        header : req.body.blogHeader,
+        text : req.body.blogText
+    }
+
+
+    let headerLink = post.header.replace(/ /g,"-");
+
+
+
+    posts.push(post);
+    res.redirect("/");
 })
+
+
 app.get("/about",function(req,res){
     res.send("Yellooooo")
 })
 app.get("/contact",function(req,res){
     res.send("Yellooooo")
 })
-app.get("/compose",function(req,res){
+app.get(["/compose" ,"/posts/compose" , "/about/compose" ,"/contact/compose"],function(req,res){
+
+    console.log("*************************************************");
+    console.log(req.query)
+    console.log(req.route);
     res.render("compose")
 })
-app.post("/compose",function(req,res){
-    let header = req.body.blogHeader;
-    let text = req.body.blogText;
-    blogHeader.push(header);
-    blogText.push(text);
-    res.redirect("/");
-})
-app.post("/",function(req,res){
-    console.log(req.body);
-    for(let i=0 ; i<blogHeader.length ; i++){
-        if(req.body.blog[i]==='on'){
-        console.log("YES");
-       }else{
-        console.log("NO");
-       }
-        }
+// app.post("/",function(req,res){
+//     console.log(req.body);
+//     for(let i=0 ; i<blogHeader.length ; i++){
+//         if(req.body.blog[i]==='on'){
+//         console.log("YES");
+//         document.querySelectorAll(".blogContainer")[i].classList.addClass("invisible");
+//        }else{
+//         console.log("NO");
+//        }
+//         }
     
 
 
-    res.redirect("/");
-})
+//     res.redirect("/");
+// })
+
+app.get("/posts/:postHeader" , function(req,res){
+    let requestedTitle = req.params.postHeader.replace( /-/g," " );
+    // console.log(requestedTitle);
+    posts.forEach(function(post){
+        let refactoredPostHeader = post.header.toLowerCase().replace( /[^a-zA-Z0-9-" " ]/g,"" );
+        if(refactoredPostHeader===requestedTitle){
+            res.render("Blog",{post:post});
+        }
+    })
+
+    })
